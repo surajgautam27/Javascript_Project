a2810f39e7317d7c75f644236bbc24be85dc4844
Suraj <surajgautam1111@gmai;l.com>
2023-05-01 15:19:07 +0545

    some project completed

---
+++ Notes Js/script.js
@@ -0,0 +1,121 @@
+const cardsContainer =document.getElementById('cards-container')
+const prevBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+const currentE1 = document.getElementById('current')
+const  showBtn= document.getElementById('show')
+const  hideBtn= document.getElementById('hide')
+const questionE1 = document.getElementById('question')
+const  answerE1= document.getElementById('answer')
+const  addCardBtn= document.getElementById('add-card')
+const  clearBtn= document.getElementById('clear')
+const  addContainer= document.getElementById('add-container')
+
+let currentActiveCard =0;
+
+const cardE1=[];
+
+const cardsData=getCardsData()
+
+function createCards(){
+    cardsData.forEach((data,index)=>createCard(data,index))
+}
+
+function createCard(data,index){
+    const card = document.createElement('div')
+    card.classList.add('card')
+    if(index===0){
+        card.classList.add('active')
+    }
+    card.innerHTML = `
+    <div class="inner-card">
+    <div class="inner-card-front">
+      <p>
+        ${data.question}
+      </p>
+    </div>
+    <div class="inner-card-back">
+      <p>
+        ${data.answer}
+      </p>
+    </div>
+  </div>
+    `;
+   card.addEventListener('click',()=> card.classList.toogle('show-answer'))
+  cardsEl.push(card);
+   cardsContainer.appendChild(card)
+   updateCurrentText();
+}
+function updateCurrentText(){
+    currentE1.innerText=`${currentActiveCard+1}/${cardE1.length}`
+
+}
+function getCardsData(){
+    const cards = JSON.parse(localStorage.getItem('cards'))
+    return cards=== null ? [] : cards;
+}
+
+function setCardsData(cards)
+{
+    localStorage.setItem('cards',JSON.stringify(cards))
+    window.location.reload()
+}
+createCards()
+
+nextBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card left';
+
+  currentActiveCard = currentActiveCard + 1;
+
+  if (currentActiveCard > cardsEl.length - 1) {
+    currentActiveCard = cardsEl.length - 1;
+  }
+
+  cardsEl[currentActiveCard].className = 'card active';
+
+  updateCurrentText();
+});
+
+
+prevBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card right';
+
+  currentActiveCard = currentActiveCard - 1;
+
+  if (currentActiveCard < 0) {
+    currentActiveCard = 0;
+  }
+
+  cardsEl[currentActiveCard].className = 'card active';
+
+  updateCurrentText();
+});
+showBtn.addEventListener('click',()=>{
+    addContainer.classList.add('show')
+})
+hideBtn.addEventListener('click',()=>{
+    addContainer.classList.remove('show')
+})
+
+addCardBtn.addEventListener('click', () => {
+    const question = questionE1.value;
+    const answer = answerE1.value;
+  
+    if (question.trim() && answer.trim()) {
+      const newCard = { question, answer };
+  
+      createCards(newCard);
+  
+      questionE1.value = '';
+      answerE1.value = '';
+  
+      addContainer.classList.remove('show');
+  
+      cardsData.push(newCard);
+      setCardsData(cardsData);
+    }
+  });
+clearBtn.addEventListener('click',()=>{
+    localStorage.clear();
+    cardsContainer.innerHTML='';
+    window.location.reload();
+})
---
+++ Typing Game/script.js
@@ -0,0 +1,111 @@
+const word=document.getElementById('word')
+const text=document.getElementById('text')
+const scoreE1=document.getElementById('score')
+const timeE1=document.getElementById('time')
+const endgameE1=document.getElementById('end-game-container')
+const settingsBtn=document.getElementById('settings-btn')
+const settings=document.getElementById('settings')
+const settingsForm=document.getElementById('settings-Form')
+const difficultySelect=document.getElementById('difficulty')
+
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'highfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving'
+  ];
+  let randomWord;
+
+  let score = 0 ;
+
+  let time = 10;
+
+  let difficulty=
+  localStorage.getItem('difficulty')!==null? localStorage.getItem('difficulty'):'medium';
+
+  difficultySelect.value=
+  localStorage.getItem('difficulty')!==null?
+  localStorage.getItem('difficulty')
+  :'medium';
+
+  text.focus();
+
+  const timeInterval =setInterval(updateTime,1000)
+
+  function getRandomWord(){
+    return words[Math.floor(Math.random()*words.length)]
+  }
+
+function addWordToDOM(){
+    randomWord=getRandomWord();
+    word.innerHTML=randomWord;
+}
+function updateScore(){
+    score++;
+    scoreE1.innerHTML=score;
+}
+
+function updateTime(){
+    time--;
+    timeE1.innerHTML=time+'s';
+    if(time===0){
+        clearInterval(timeInterval);
+        gameOver();
+    }
+}
+function gameOver(){
+    endgameE1.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameE1.style.display = 'flex';
+}
+addWordToDOM();
+text.addEventListener('click',()=>{
+
+    const insertedText= e.target.value;
+    if(insertedText===randomWord)
+    {
+        addWordToDOM();
+        updateScore();
+        e.target.value='';
+
+        if(difficulty==='hard')
+        {
+            time+=2;
+        }
+        else if(difficulty==='medium')
+        {
+            time +=3;
+        }
+        else{
+            time +=5;
+        }
+        updateTime()
+    }
+})
+settingsBtn.addEventListener('click',()=>{
+    settings.classList.toggle('hide');
+})
+settingsForm.addEventListener('change', (e) => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
+  });
